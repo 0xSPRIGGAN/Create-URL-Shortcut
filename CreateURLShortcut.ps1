47f36c203deaaf2b78956ccd707d2937331a89d7
Connor Hudson <36829070+cqhudson@users.noreply.github.com>
2022-09-12 12:12:55 -0500

    Add files via upload

---
+++ CreateURLShortcut.ps1
@@ -0,0 +1,27 @@
+﻿# 
+# Author: Connor Hudson
+# Date: 12-Sept-2022
+# 
+
+<#
+# NOTE: Be sure to add a '\' char either at the beginning of $ShortcutName,
+# or at the end of $ShortcutPath.
+# 
+# For example: $ShortcutName = "\Google Search.url"
+#              $ShortcutPath = "C:\Users\admin\Desktop"
+#       
+#              OR
+#              
+#              $ShortcutName = "Google Search.url"
+#              $ShortcutPath = "C:\Users\admin\Desktop\"
+#>
+
+$ShortcutName = ""
+$ShortcutPath = ""
+$URL = ""
+
+$Shell = New-Object -ComObject ("WScript.Shell")
+
+$Shortcut = $Shell.CreateShortcut($ShortcutPath + $ShortcutName)
+$Shortcut.Targetpath = $URL
+$Shortcut.Save()
